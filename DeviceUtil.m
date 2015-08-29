fc2e5aee4453599a42edf7dc4292c41317c360cb
Gertjan Reynaert <gertjan@typework.com>
2015-08-29 11:22:28 +0200

    Fix enum values

--- DeviceUtil.m
+++ DeviceUtil.m
@@ -17,13 +17,13 @@ RCT_EXPORT_MODULE();
     NSString *systemVersion = device.systemVersion;
     NSString *localizedModel = device.localizedModel;
     bool multitaskingSupported = device.multitaskingSupported;
-    UIUserInterfaceIdiom userInterfaceIdiom = device.userInterfaceIdiom;
-    NSUUID *identifierForVendor = device.identifierForVendor;
-    UIDeviceOrientation orientation = device.orientation;
+    NSString *userInterfaceIdiom = self.userInterfaceIdiom;
+    NSString *identifierForVendor = device.identifierForVendor.UUIDString;
+    NSString *orientation = self.orientation;
     bool generatesDeviceOrientationNotifications = device.generatesDeviceOrientationNotifications;
     float batteryLevel = device.batteryLevel;
     bool batteryMonitoringEnabled = device.batteryMonitoringEnabled;
-    UIDeviceBatteryState batteryState = device.batteryState;
+    NSString *batteryState = self.batteryState;
     bool proximityMonitoringEnabled = device.proximityMonitoringEnabled;
     bool proximityState = device.proximityState;
 
@@ -34,16 +34,91 @@ RCT_EXPORT_MODULE();
              @"systemVersion" : (systemVersion),
              @"multitaskingSupported" : ([NSNumber numberWithBool:multitaskingSupported]),
              @"localizedModel" : (localizedModel),
-             @"userInterfaceIdiom" : ([NSNumber numberWithInt:userInterfaceIdiom]),
+             @"userInterfaceIdiom" : (userInterfaceIdiom),
              @"identifierForVendor" : (identifierForVendor),
-             @"orientation" : ([NSNumber numberWithInt:orientation]),
+             @"orientation" : (orientation),
              @"generatesDeviceOrientationNotifications" : ([NSNumber numberWithBool:generatesDeviceOrientationNotifications]),
              @"batteryLevel" : ([NSNumber numberWithFloat:batteryLevel]),
              @"batteryMonitoringEnabled" : ([NSNumber numberWithBool:batteryMonitoringEnabled]),
-             @"batteryState" : ([NSNumber numberWithInt:batteryState]),
+             @"batteryState" : (batteryState),
              @"proximityMonitoringEnabled" : ([NSNumber numberWithBool:proximityMonitoringEnabled]),
              @"proximityState" : ([NSNumber numberWithBool:proximityState]),
     };
 }
 
+- (NSString *)userInterfaceIdiom {
+  UIDevice *device = [UIDevice currentDevice];
+
+  switch (device.userInterfaceIdiom) {
+    case UIUserInterfaceIdiomPad:
+      return @"Pad";
+      break;
+
+    case UIUserInterfaceIdiomPhone:
+      return @"Phone";
+      break;
+
+    default:
+      return @"Unspecified";
+      break;
+  }
+}
+
+- (NSString *)batteryState {
+  UIDevice *device = [UIDevice currentDevice];
+
+  switch (device.batteryState) {
+    case UIDeviceBatteryStateCharging:
+      return @"Charging";
+      break;
+
+    case UIDeviceBatteryStateFull:
+      return @"Full";
+      break;
+
+    case UIDeviceBatteryStateUnplugged:
+      return @"Unplugged";
+      break;
+
+    default:
+      return @"Unknown";
+      break;
+  }
+}
+
+- (NSString *)orientation {
+  UIDevice *device = [UIDevice currentDevice];
+
+  switch (device.orientation) {
+    case UIDeviceOrientationFaceDown:
+      return @"FaceDown";
+      break;
+
+    case UIDeviceOrientationFaceUp:
+      return @"FaceUp";
+      break;
+
+    case UIDeviceOrientationLandscapeLeft:
+      return @"LandscapeLeft";
+      break;
+
+    case UIDeviceOrientationLandscapeRight:
+      return @"LandscapeRight";
+      break;
+
+    case UIDeviceOrientationPortrait:
+      return @"Portrait";
+      break;
+
+    case UIDeviceOrientationPortraitUpsideDown:
+      return @"UpsideDown";
+      break;
+
+    default:
+      return @"Unknown";
+      break;
+  }
+}
+
 @end
+
