0222d1dd9bd3eb7f223fc08eb9a1057af0ee0ad5
Gertjan Reynaert <gertjan@typework.com>
2015-08-29 09:48:32 +0200

    Add all UIDevice properties [not tested yet]

--- Device.js
+++ Device.js
@@ -9,14 +9,25 @@ class Device {
     this.deviceName = DeviceUtil.name;
     this.systemName = DeviceUtil.systemName;
     this.systemVersion = DeviceUtil.systemVersion;
+    this.multitaskingSupported = DeviceUtil.multitaskingSupported;
+    this.localizedModel = DeviceUtil.localizedModel;
+    this.userInterfaceIdiom = DeviceUtil.userInterfaceIdiom;
+    this.identifierForVendor = DeviceUtil.identifierForVendor;
+    this.orientation = DeviceUtil.orientation;
+    this.generatesDeviceOrientationNotifications = DeviceUtil.generatesDeviceOrientationNotifications;
+    this.batteryLevel = DeviceUtil.batteryLevel;
+    this.batteryMonitoringEnabled = DeviceUtil.batteryMonitoringEnabled;
+    this.batteryState = DeviceUtil.batteryState;
+    this.proximityMonitoringEnabled = DeviceUtil.proximityMonitoringEnabled;
+    this.proximityState = DeviceUtil.proximityState;
   }
 
   isIpad() {
-    return this.model.indexOf('iPad') >= 0;
+    return this.userInterfaceIdiom === 'Pad';
   }
 
   isIphone() {
-    return this.model.indexOf('iPhone') >= 0;
+    return this.userInterfaceIdiom === 'Phone';
   }
 }
 
--- DeviceUtil.m
+++ DeviceUtil.m
@@ -9,17 +9,41 @@ RCT_EXPORT_MODULE();
 @synthesize bridge = _bridge;
 
 - (NSDictionary *)constantsToExport {
-    NSString *model = [[UIDevice currentDevice] model];
-    NSString *name = [[UIDevice currentDevice] name];
-    NSString *systemName = [[UIDevice currentDevice] systemName];
-    NSString *systemVersion = [[UIDevice currentDevice] systemVersion];
+    UIDevice *device = [UIDevice currentDevice];
+
+    NSString *model = device.model;
+    NSString *name = device.name;
+    NSString *systemName = device.systemName;
+    NSString *systemVersion = device.systemVersion;
+    NSString *localizedModel = device.localizedModel;
+    bool multitaskingSupported = device.multitaskingSupported;
+    UIUserInterfaceIdiom userInterfaceIdiom = device.userInterfaceIdiom;
+    NSUUID *identifierForVendor = device.identifierForVendor;
+    UIDeviceOrientation orientation = device.orientation;
+    bool generatesDeviceOrientationNotifications = device.generatesDeviceOrientationNotifications;
+    float batteryLevel = device.batteryLevel;
+    bool batteryMonitoringEnabled = device.batteryMonitoringEnabled;
+    UIDeviceBatteryState batteryState = device.batteryState;
+    bool proximityMonitoringEnabled = device.proximityMonitoringEnabled;
+    bool proximityState = device.proximityState;
 
     return @{
              @"model" : (model),
              @"name" : (name),
              @"systemName" : (systemName),
-             @"systemVersion" : (systemVersion)
-             };
+             @"systemVersion" : (systemVersion),
+             @"multitaskingSupported" : (multitaskingSupported),
+             @"localizedModel" : (localizedModel),
+             @"userInterfaceIdiom" : (userInterfaceIdiom),
+             @"identifierForVendor" : (identifierForVendor),
+             @"orientation" : (orientation),
+             @"generatesDeviceOrientationNotifications" : (generatesDeviceOrientationNotifications),
+             @"batteryLevel" : (batteryLevel),
+             @"batteryMonitoringEnabled" : (batteryMonitoringEnabled),
+             @"batteryState" : (batteryState),
+             @"proximityMonitoringEnabled" : (proximityMonitoringEnabled),
+             @"proximityState" : (proximityState),
+    };
 }
 
 @end
