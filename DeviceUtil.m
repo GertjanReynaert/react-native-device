44d17ff27190e931d83f39fa6ba321acc7744987
Justin Hamade <justinffs@users.noreply.github.com>
2017-08-10 09:21:42 -0700

    Support React Native headers for 0.40+
    Also provide backwards compatibility for <=0.39

--- DeviceUtil.m
+++ DeviceUtil.m
@@ -1,6 +1,16 @@
 #import "DeviceUtil.h"
+// Support React Native headers both in the React namespace, where they are in RN version 0.40+,
+// and no namespace, for older versions of React Native
+#if __has_include(<React/RCTBridge.h>)
 #import <React/RCTBridge.h>
+#else
+#import "RCTBridge.h"
+#endif
+#if __has_include(<React/RCTUtils.h>)
 #import <React/RCTUtils.h>
+#else
+#import "RCTUtils.h"
+#endif
 #import <sys/utsname.h>
 
 @implementation DeviceUtil
